f3606e009ee8124eeebdc06e29d6587b9cbc6b52
Marcelo Melo <102917176+MarcelloMello90@users.noreply.github.com>
2022-12-30 22:19:27 +0100

    Add files via upload

--- jogo-adivinhacao/main.js
+++ jogo-adivinhacao/main.js
@@ -9,18 +9,22 @@ let xAttempts = 1
 // Eventos
 btnTry.addEventListener('click', handleTryClick)
 btnReset.addEventListener('click', handleResetClick)
-document.addEventListener('keydown', function(e) {
-    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
-        handleResetClick()
-    }
-})
+document.addEventListener('keydown', handleAdvance)
 
 // funções
 function handleTryClick(event) {
-    event.preventDefault() // nao faca o padrao
+    event.preventDefault()
 
     const inputNumber = document.querySelector("#inputNumber")
 
+    if (inputNumber.value == ""){
+        return
+    }
+
+    if(Number(inputNumber.value) <= 0 || Number(inputNumber.value >10 )){
+        alert("Digite um valor entre 1 e 10")
+    }
+    
     if(Number(inputNumber.value) == randomNumber) {
         toggleScreen()
         screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas`
@@ -39,4 +43,10 @@ function handleResetClick() {
 function toggleScreen() {
     screen1.classList.toggle("hide")
     screen2.classList.toggle("hide")
+}
+
+function handleAdvance() {
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
+}
 }
